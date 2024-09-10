9c87789dd700c7a67925c59d84692a76f1d4d0a2
Camille Christie <112962852+camchris@users.noreply.github.com>
2024-09-09 20:12:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/helloworldbutton/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.helloworldbutton
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworldbutton.ui.theme.HelloWorldButtonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldButtonTheme {
+
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    ClickButton(context = this@MainActivity)
+
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ClickButton(context: MainActivity) {
+
+    Button(
+        onClick = {
+            Toast.makeText(context, "Hello World!", Toast.LENGTH_SHORT).show()
+        }
+    ) {
+        Text("Click me")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldButtonTheme {
+        Greeting("Android")
+    }
+}
